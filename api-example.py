0d74d89bb5c54cd9d342a607beac56607c580f62
ako111 <ales.koprivnikar@gmail.com>
2016-10-31 09:09:58 -0700

    Cosmetic changes
    Code review and some minor changes for clarity.

--- api-example.py
+++ api-example.py
@@ -390,11 +390,12 @@ if exampleNo == 3:
 
 """
     Example #4
-    In this example we take the DNS Trace test data and aggregate the mappings
-    per agent over time in a form of an CSV file (created in working directory).
-    The test in question has to be configured to get A only A records.
-    Agents are specified in column A, then for each time period a column is created
-    and all of the mappings are semi colon separated in the the cell.
+    In this example we take the DNS Trace test data in a time window (parameter)
+    and aggregate the mappings per agent over time (in time periods - parameter)
+    in a form of an CSV file which gets created in working directory.
+    The test in question has to be configured to get only A records.
+    Agents are specified in first column, then for each time period a column is created
+    and all of the mappings are semicolon separated in the cell.
 
     Use case: Track potential changes to mappings over time from multiple locations
     to verify geo load balancing
@@ -408,33 +409,33 @@ if exampleNo == 3:
 """
 
 def loadTestData(api, testId):
-    """ Get the test data from the API for the test ID and given parameters """
+    """ Get the test data from the API for the test ID and given parameters. """
     testData = api.getRequest('/dns/trace/' + str(testId) + '.json', uriParameters)
 
-    """ Check if we have a DNS trace test on our hands otherwise we exit """
+    """ Check if we have a DNS trace test on our hands otherwise we exit. """
     if not (testData['dns']['test']['type'] == 'dns-trace'):
         sys.exit('This example requires a DNS trace test.')
     """
     Check if really have an A record test on our hands otherwise we exit.
-    The domain field specifies domain under test and record type
+    The domain field specifies domain under test and record type.
     """
     if not (testData['dns']['test']['domain'][-2:] == ' A'):
-        sys.exit('This example requires a DNS trace test for A record')
+        sys.exit('This example requires a DNS trace test for A record.')
 
-    """ Build a list of all traces for the test in the time window """
+    """ Build a list of all traces for the test in the time window. """
     traces = list()
     traces.extend(testData['dns']['trace'])
 
     """
-    Now check if have multiple pages and add all of them to traces
-    If a "next" field is present in "pages" we have another page
-    and the "next" field provides a full URL for it
+    Now check if have multiple pages and add all of them to traces.
+    If a "next" field is present in "pages" we have another page and the "next"
+    field provides a full URL for it.
     """
     while 'next' in testData['pages'].keys():
-        """ Show progress """
+        """ Show progress. """
         sys.stdout.write('.')
         sys.stdout.flush()
-        """ Call the API again with next page URL provided in the response """
+        """ Call the API again with next page URL provided in the response. """
         testData = api.getPureUrlRequest(testData['pages']['next'])
         traces.extend(testData['dns']['trace'])
     print '\n'
@@ -446,17 +447,17 @@ def processTraces(traces):
 
     for trace in traces:
 
-        """ Get the date from the trace date/time in epoch """
+        """ Get the date from the trace date/time in epoch. """
         traceEpoch = calendar.timegm(time.strptime (trace['date'], '%Y-%m-%d %H:%M:%S'))
 
-        """ Calculated time period ID """
+        """ Calculated time period ID. """
         timePeriodId = (traceEpoch - startWindowTimeEpoch) / timePeriodSec
         if timePeriodId == -1:
             """
             Because the API returns all the rounds that are within the window, we
             can possiblly get a round which is before our calculated window.
-            In this case we get a -1 time period id, which we can simply add to the
-            first period (timePeriodId = 0)
+            In this case we get a -1 time period ID, which we add to the first
+            period (timePeriodId = 0) for simplification.
             """
             timePeriodId = 0
         """
@@ -467,15 +468,17 @@ def processTraces(traces):
             data[trace['agentName']] = {}
         """
         Check if the time period exists for the agent and add the current
-        one into a map for the period. Simple way to avoid duplication.
+        mapping into a map for the period. It's a simple way to avoid duplication
+        of the mappings and avoid a flood of data if we specify larger time
+        windows.
         """
         if timePeriodId not in data[trace['agentName']].keys():
             data[trace['agentName']][timePeriodId] = {}
-        """ If a trace fails, we get an errorDetails field in the JSON """
-        if not'errorDetails' in trace.keys():
+        """ If a trace fails, we get an errorDetails field in the JSON. """
+        if not 'errorDetails' in trace.keys():
             data[trace['agentName']][timePeriodId][trace['mappings']] = ''
         else:
-            """ To make it clear there was an error lets put a key in the map """
+            """ To make it clear there was an error lets put a key in the map. """
             data[trace['agentName']][timePeriodId]['ERROR'] = ''
     return data
 
@@ -485,15 +488,15 @@ def generateCSV(fName, data):
     try:
         csvWriter = csv.writer(f)
         csvWriter.writerow(['Output'])
-        """ Handle the labels for the columns and tag them with start time """
+        """ Handle the labels for the columns and tag them with start time. """
         isFirstRow = True
         firstRow = ['']
         for agent in data:
-            """ Build a row to be written """
+            """ Build a row to be written. """
             row = []
             row.extend([agent])
             for period in data[agent]:
-                """ Gather the start times for the periods and format """
+                """ Gather the start times for the periods and format. """
                 if isFirstRow:
                     firstRow.extend([time.strftime('%x %X', time.gmtime(startWindowTimeEpoch + period * timePeriodSec))])
 
@@ -502,18 +505,17 @@ def generateCSV(fName, data):
                 for mapping in data[agent][period].keys():
                     """ Concat mappings and ; separate. """
                     mapjoin += mapping + ';'
-                """ Remove the last ; """
+                """ Remove the last ;. """
                 mapjoin = mapjoin[:-1]
                 row.extend([mapjoin])
 
 
-            """ Handle the first row """
+            """ Handle the first row. """
             if isFirstRow:
                 csvWriter.writerow(firstRow)
                 isFirstRow = False
-            """ Write the row """
+            """ Write the row. """
             csvWriter.writerow(row)
-
     finally:
         f.close()
 
@@ -554,10 +556,10 @@ if exampleNo == 4:
 
     """
     Go over the traces and split them into a new multi dimensional map where:
-    -agentName defines the key for map of agents
-    -timeperiod defines the the key for a map containing list of mappings
+    -agentName defines the key for map of agents.
+    -timeperiod defines the the key for a map containing list of mappings.
     -mappings list will contain all the mappings received by the agent in the
-        traces that fall into a time period
+        traces that fall into a time period.
 
     Time period is calculated in the following way:
     (traceEpoch - startWindowTimeEpoch) / timeWindowSec
@@ -567,13 +569,14 @@ if exampleNo == 4:
 
     """
     This is where we build the output CSV file.
-    Agents are listed in first column
+    Agents are listed in first column.
     Each time period for each agent has column as well.
-    Each cell gets the mappings for the particular time period.
+    Each cell gets the mappings for a particular time period.
 
-    Go over the agents and then for each agent time period put the mappings into a
+    Go over the agents and then for each agent/time period put the mappings
+    into a row.
     """
-    """ File name """
+    """ File name. """
     fName = 'output'+str(endWindowTimeEpoch)+'.csv'
 
     generateCSV(fName, data)
